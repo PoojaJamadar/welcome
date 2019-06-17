1e052ee97704e17eb65cfa137602a4bca9f7c311
PoojaJamadar <51926186+PoojaJamadar@users.noreply.github.com>
2019-06-17 23:28:35 +0530

    student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student
+{
+	int id;
+	String name;
+
+	public static void main(String[] args)
+	{
+		Student s=new Student();
+		System.out.println(s.id);//0
+		System.out.println(s.name);//null,giving default value
+	}
+
+}
